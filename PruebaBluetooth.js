75afcdc44fca741b9d9635dc803bc9a5dbdb3382
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2014-01-02 18:34:32 -0300

    agregado boton buscar

--- PruebaBluetooth.js
+++ PruebaBluetooth.js
@@ -1,13 +1,19 @@
 var onDeviceReady = function() {     
-    var vista_devices = $("#devices");    
-    bluetoothSerial.list(function(devices){
-        vista_devices.text(JSON.stringify(devices));
-    }, function(){
-        vista_devices.text('error al obtener lista de dispositivos');
+    var vista_devices = $("#devices"); 
+    var btn_buscar = $("#btnBuscar");
+    btn_buscar.click(function(){
+        vista_devices.text("buscando dispositivos...");
+        bluetoothSerial.list(function(devices){
+            vista_devices.text(JSON.stringify(devices));
+        }, function(){
+            vista_devices.text('error al obtener lista de dispositivos');
+        });
     });
+    vista_devices.text("presione el boton buscar");
 };
 
 $(document).ready(function() {  
+    console.log("comenzando aplicacion de prueba bluetooth");
     // are we running in native app or in browser?
     window.isphone = false;
     if(document.URL.indexOf("file://") == -1) {
