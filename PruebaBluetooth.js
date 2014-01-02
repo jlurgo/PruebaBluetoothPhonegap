ac6b49c1bad554ab7cf115e1339182567af2cfd8
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2014-01-02 19:48:20 -0300

    agregado boton buscar

--- PruebaBluetooth.js
+++ PruebaBluetooth.js
@@ -1,15 +1,34 @@
 var onDeviceReady = function() {     
     var vista_devices = $("#devices"); 
-    var btn_buscar = $("#btnBuscar");
-    btn_buscar.click(function(){
+    var vista_mensajes = $("#mensajes"); 
+    var btnBuscar = $("#btnBuscar");
+    var btnConectar = $("#btnConectar");
+    
+    btnBuscar.click(function(){
         vista_devices.text("buscando dispositivos...");
         bluetoothSerial.list(function(devices){
-            vista_devices.text(JSON.stringify(devices));
+            vista_devices.text(devices.lenght);
         }, function(){
             vista_devices.text('error al obtener lista de dispositivos');
         });
     });
+    
+    btnConectar.click(function(){
+        vista_devices.text("conectando...");
+        bluetoothSerial.connect('linvor', 
+            function(){
+                vista_devices.text('conectado a linvor');
+            }, function(){
+                vista_devices.text('error al conectar');
+        });
+    });
+    
     vista_devices.text("presione el boton buscar");
+    
+    // the success callback is called whenever data is received
+    bluetoothSerial.subscribe('\n', function (data) {
+        vista_mensajes.text(vista_devices.text() + JSON.stringify(data));
+    }, failure);
 };
 
 $(document).ready(function() {  
